c9166952129832f06f923b4fb6cfbad246689c2e
wangdi <wangdi@yaeher.com>
2021-01-13 15:43:16 +0800

    reset

--- src/main/java/headfirst/component/trys/NormalMenu.java
+++ src/main/java/headfirst/component/trys/NormalMenu.java
@@ -20,13 +20,8 @@ public class NormalMenu extends DefaultComponent {
     private String description;
 
     public List<DefaultComponent> getSubList() {
-        System.out.println("测试冲突到底怎么解决");
-        System.out.println("测试冲突到底怎么解决");
-        System.out.println("测试冲突到底怎么解决");
-        System.out.println("测试冲突到底怎么解决");
         return subList;
     }
-
 
     public NormalMenu(String name, String description) {
         this.name = name;
